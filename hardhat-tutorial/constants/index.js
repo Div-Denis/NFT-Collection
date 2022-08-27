66fe866ccb72d9a068bebda7a881842ec26434a7
Div-Denis <82740084+Div-Denis@users.noreply.github.com>
2022-08-28 07:04:16 +0800

    Update index.js

--- hardhat-tutorial/constants/index.js
+++ hardhat-tutorial/constants/index.js
@@ -1,6 +1,6 @@
 //部署的白名单地址
 const WHITELIST_CONTRACT_ADDRESS = "0x20A8e6Be0f9824D885d9479dfAA46F807e24B5b9";
 //在metadata中的CRYPTO Dev NFT 的URL
-const METADATA_URL ="http://nft-collection-sneh1999.vercel.app/api/" ;
+const METADATA_URL ="https://nft-collection-rho-rouge.vercel.app/api/";
 
   module.exports = { WHITELIST_CONTRACT_ADDRESS, METADATA_URL };
